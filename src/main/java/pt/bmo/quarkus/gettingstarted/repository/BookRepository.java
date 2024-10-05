b2b3e1a186cb74d70fb7ea0ced1ed500b7fe8ae3
Bruno Moreno <brunomorello7@gmail.com>
2024-10-05 11:34:35 +0100

    Added ConfigProperty using injection and customized quarkus config

--- src/main/java/pt/bmo/quarkus/gettingstarted/repository/BookRepository.java
+++ src/main/java/pt/bmo/quarkus/gettingstarted/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package pt.bmo.quarkus.gettingstarted.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import pt.bmo.quarkus.gettingstarted.model.Book;
 
 import java.util.List;
@@ -8,12 +9,16 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
+
+    @ConfigProperty(name = "books.genre", defaultValue = "Others")
+    private String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "understanding quarkus", "Antonio", 2020, "IT"),
-                new Book(2, "test 2", "Antonio", 2020, "IT"),
-                new Book(3, "test3", "Antonio", 2020, "IT"),
-                new Book(4, "true at first light", "hemingway", 1999, "novel")
+                new Book(1, "understanding quarkus", "Antonio", 2020, genre),
+                new Book(2, "test 2", "Antonio", 2020, genre),
+                new Book(3, "test3", "Antonio", 2020, genre),
+                new Book(4, "true at first light", "hemingway", 1999, genre)
         );
     }
 
